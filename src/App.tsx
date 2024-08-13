144aa8276b2a5a7a84b4f549115595753853a605
anton <avainfoconseils@gmail.com>
2024-08-13 21:10:14 +0200

    feat(project): add sass to project, add basic style and profile picture
    Added the base of the main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
+import './App.scss';
 
 function App() {
 	return (
 		<div className="app">
+			<link rel="preconnect" href="https://fonts.googleapis.com"/>
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+			<link
+				href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+				rel="stylesheet"/>
+			<div className="mainPage">
+				<div className="header">
+					<div className="left">
+					</div>
+					<div className="right">
+					</div>
+				</div>
+				<div className="content"></div>
+			</div>
+			<div className="infos"></div>
+			<div className="projects"></div>
 		</div>
 	);
 }
