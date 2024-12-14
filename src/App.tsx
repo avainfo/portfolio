5f19291b1f5fafb85865997f8cc5d4c2a752a7ca
anton <avainfoconseils@gmail.com>
2024-12-14 16:07:29 +0100

    refactor(scroll): disable scroll events

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import {HeaderTitle} from "./components/header/HeaderTitle";
 import {Header} from "./components/header/Header";
 import {Links} from "./components/utils/Links";
-import {loadEvents} from "./script/eventListener";
 import {MainPage} from "./components/pages/MainPage";
 import {InfosPage} from "./components/pages/InfosPage";
 
@@ -16,32 +15,43 @@ function App() {
 	];
 
 	const [backgroundUrl, setBackgroundUrl] = useState("/assets/card-front.png");
-	const [currentScrollPos, setCurrentScrollPos] = useState(0);
-
-	loadEvents();
-
-	const handleScroll = (event: any) => {
-		event.preventDefault();
-		const viewportHeight = window.innerHeight;
-		let newScrollPos = currentScrollPos;
-
-		if (event.deltaY > 0) {
-			newScrollPos = Math.min(currentScrollPos + viewportHeight, document.body.scrollHeight - viewportHeight);
-		} else {
-			newScrollPos = Math.max(currentScrollPos - viewportHeight, 0);
-		}
-
-		setCurrentScrollPos(newScrollPos);
-		window.scrollTo({
-			top: newScrollPos,
-			behavior: 'smooth'
-		});
-	};
-
-	useEffect(() => {
-		window.addEventListener('wheel', handleScroll, {passive: false});
-		return () => window.removeEventListener('wheel', handleScroll);
-	}, [currentScrollPos]);
+	// const currentScrollPos = useRef(0);
+	// const isScrolling = useRef(false);
+	//
+	// loadEvents();
+	//
+	// const handleScroll = (event: any) => {
+	// 	event.preventDefault();
+	//
+	// 	if (isScrolling.current) return;
+	//
+	// 	isScrolling.current = true;
+	// 	const viewportHeight = window.innerHeight;
+	// 	let newScrollPos = currentScrollPos.current;
+	//
+	// 	if (event.deltaY > 0) {
+	// 		newScrollPos = Math.min(currentScrollPos.current + viewportHeight, document.body.scrollHeight - viewportHeight);
+	// 	} else {
+	// 		console.log()
+	// 		newScrollPos = Math.max(currentScrollPos.current - viewportHeight, 0);
+	// 	}
+	//
+	// 	currentScrollPos.current = newScrollPos;
+	//
+	// 	setTimeout(() => {
+	// 		isScrolling.current = false;
+	// 	}, 500);
+	//
+	// 	window.scrollTo({
+	// 		top: newScrollPos,
+	// 		behavior: 'smooth'
+	// 	});
+	// };
+	//
+	// useEffect(() => {
+	// 	window.addEventListener('wheel', handleScroll, {passive: false});
+	// 	return () => window.removeEventListener('wheel', handleScroll);
+	// }, []);
 
 	return (
 		<div className="app">
