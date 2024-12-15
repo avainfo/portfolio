7dc26242f44b746105fb9980d31687f563214812
anton <avainfoconseils@gmail.com>
2024-12-15 16:29:05 +0100

    feat(routes): create routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,68 +7,38 @@ import {MainPage} from "./pages/MainPage";
 import {InfosPage} from "./pages/InfosPage";
 import {ProjectPage} from "./pages/ProjectPage";
 import {FormationPage} from "./pages/FormationPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-	let header: Array<string> = [
-		"Home",
-		"About",
-		"Projects",
-		"Contacts",
-	];
+    let header: Array<string> = [
+        "Home",
+        "About",
+        "Projects",
+        "Contacts",
+    ];
 
-	const [backgroundUrl, setBackgroundUrl] = useState("/assets/card-front.png");
-	// const currentScrollPos = useRef(0);
-	// const isScrolling = useRef(false);
-	//
-	// loadEvents();
-	//
-	// const handleScroll = (event: any) => {
-	// 	event.preventDefault();
-	//
-	// 	if (isScrolling.current) return;
-	//
-	// 	isScrolling.current = true;
-	// 	const viewportHeight = window.innerHeight;
-	// 	let newScrollPos = currentScrollPos.current;
-	//
-	// 	if (event.deltaY > 0) {
-	// 		newScrollPos = Math.min(currentScrollPos.current + viewportHeight, document.body.scrollHeight - viewportHeight);
-	// 	} else {
-	// 		console.log()
-	// 		newScrollPos = Math.max(currentScrollPos.current - viewportHeight, 0);
-	// 	}
-	//
-	// 	currentScrollPos.current = newScrollPos;
-	//
-	// 	setTimeout(() => {
-	// 		isScrolling.current = false;
-	// 	}, 500);
-	//
-	// 	window.scrollTo({
-	// 		top: newScrollPos,
-	// 		behavior: 'smooth'
-	// 	});
-	// };
-	//
-	// useEffect(() => {
-	// 	window.addEventListener('wheel', handleScroll, {passive: false});
-	// 	return () => window.removeEventListener('wheel', handleScroll);
-	// }, []);
+    const [backgroundUrl, setBackgroundUrl] = useState("/assets/card-front.png");
 
-	return (
-		<div className="app">
-			<Links/>
-			<Header
-				strings={header}
-				callbackFunction={(title, index) => {
-					return <HeaderTitle title={title} key={index} scrollTo={index}/>;
-				}}/>
-			<MainPage event={(value) => setBackgroundUrl(value)} backgroundUrl={backgroundUrl}/>
-			<InfosPage/>
-			<ProjectPage/>
-			<FormationPage/>
-		</div>
-	);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={
+                    <div className="app">
+                        <Links/>
+                        <Header
+                            strings={header}
+                            callbackFunction={(title, index) => {
+                                return <HeaderTitle title={title} key={index} scrollTo={index}/>;
+                            }}/>
+                        <MainPage event={(value) => setBackgroundUrl(value)} backgroundUrl={backgroundUrl}/>
+                        <InfosPage/>
+                        <ProjectPage/>
+                        <FormationPage/>
+                    </div>
+                }/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
